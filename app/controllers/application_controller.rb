15f7aa102f90d9a66b0500ba72c80593ce1bf033
Clay Stewart <c.stewart89@gmail.com>
2021-09-23 19:27:34 -0400

    commit tree 106edf76a6942cccdeca1cddebd9554fcaa2af00

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,47 @@
-
+# frozen_string_literal: true
 require_relative '../../config/environment'
 
+# Main Controller
 class ApplicationController < Sinatra::Base
-
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  delete '/articles/:id' do
+    article = Article.find_by_id(params[:id])
+    article.destroy
+    redirect '/articles'
+  end
+
+  get '/articles/:id' do
+    @article = Article.find_by_id(params[:id])
+    erb :show
+  end
+
+  post '/articles' do
+    redirect "/articles/#{Article.find_or_create_by(params).id}"
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/articles' do
+    article = Article.find_by_id(params['id'])
+    article.title = params[:title]
+    article.content = params[:content]
+    article.save
+    redirect "/articles/#{article.id}"
   end
 end
